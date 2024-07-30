8f29bffd7b588860cf77590d26c5bfc926eef03d
Ralf Handl <ralf.handl@sap.com>
2024-07-30 12:52:42 +0200

    server and client

---
+++ client.js
@@ -0,0 +1,16 @@
+const axios = require("axios");
+
+axios
+  .request({ url: "http://localhost:3000", method: "QUERY" })
+  .then(function (response) {
+    // handle success
+    console.log(response.status);
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.all("/", (req, res) => {
+  res.send(`Hello World with ${req.method}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
